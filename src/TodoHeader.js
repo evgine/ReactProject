2271c2686a6cf37e8f3d97a3256b8ef72ef9ed8c
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-08-28 03:04:48 +0400

    Update TodoHeader.js

--- src/TodoHeader.js
+++ src/TodoHeader.js
@@ -1,19 +1,66 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function TodoHeader({onAdd}){
-    const [text, setText] = useState("");       //destructurisation
+function TodoHeader(props) {
+    const [text, setText] = useState(props.edit ? props.edit.value : "");
+
     return (
         <form onSubmit={(e) => {
             e.preventDefault();
-            onAdd(text);
+
+            props.onSubmit({
+                id: Math.random(),
+                text: text
+            });
             setText("");
         }}>
-            <input type="text" value={text} onChange={(e) => {
-                setText(e.target.value);
-            }}/>
-            <button>Add</button>
+            {props.edit ? (
+                <>
+                    <input
+                        placeholder="Update Todo"
+                        value={text}
+                        onChange={(e) => {
+                            setText(e.target.value);
+                        }}
+                        name="text"
+                    />
+                    <button onClick={(e) => {
+                        e.preventDefault();
+
+                        props.onSubmit({
+                            id: Math.random(),
+                            text: text
+                        });
+                        setText("");
+                    }}
+                    >
+                        Update
+                    </button>
+                </>
+            ) : (
+                <>
+                    <input
+                        placeholder="Add Todo"
+                        value={text}
+                        onChange={(e) => {
+                            setText(e.target.value);
+                        }}
+                        name="text"
+                    />
+                    <button onClick={(e) => {
+                        e.preventDefault();
+
+                        props.onSubmit({
+                            id: Math.random(),
+                            text: text
+                        });
+                        setText("");
+                    }}>
+                        Add todo
+                    </button>
+                </>
+            )}
         </form>
-    )
+    );
 }
 
 export default TodoHeader;
