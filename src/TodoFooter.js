bcd38a15bdfc730e56c22b65b959c4c54800084d
yevginemnatsakanyan <evgine.v.mnatsakanyan@gmail.com>
2022-08-28 03:04:09 +0400

    Update TodoFooter.js

--- src/TodoFooter.js
+++ src/TodoFooter.js
@@ -1,9 +1,12 @@
 function TodoFooter({todos}){
 
     const completedSize = todos.filter((todo) => todo.isCompleted).length;
+    const activesSize = todos.filter((todo) => !todo.isCompleted).length
     return (
         <div>
-            <span>{completedSize}Completed</span>                   
+            <span>{completedSize}Completed Todos</span>  
+            <br/> 
+            <span>{activesSize}Active Todos</span>                
         </div>
     )
 }
